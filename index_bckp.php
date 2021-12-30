13fd8855b9fc119b7492d7e27368fa06f1fe1573
rahmat-ilyas <rahmat.ilyas142@gmail.com>
2021-12-31 00:52:26 +0800

    Bismillah Add

---
+++ index_bckp.php
@@ -0,0 +1,73 @@
+<?php      
+require('layout/header.php');
+?>
+
+<section class="home_banner_area">
+	<div class="banner_inner">
+		<div class="container">
+			<div class="banner_content">
+				<p class="upper_text">SELAMAT DATANG</p>
+				<h2>DI SIMKAH</h2>
+				<p>
+					DIREKTORAT JENDERAL BIMBINGAN MASYARAKAT ISLAM
+					KEMENTERIAN AGAMA REPUBLIK INDONESIA
+				</p>
+				<!-- <a class="primary_btn mr-20" href="#">Survei</a> -->
+				<a class="primary_btn yellow_btn text-white" href="daftar.php">Pendaftaran Nikah</a>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="info_pendaftaran" id="info_pendaftaran">
+	<div class="container">
+		<h1 class="text-center" style="color: #fff; padding-top: 35px;">Info Pendaftaran Nikah</h1>
+		<div class="panel panel-default" style="margin-top: 20px;">
+			<div class="panel-heading">
+				<h4>Alur Pendaftaran</h4>
+			</div>
+			<div class="panel-body text-center">
+				<img src="img/alur.png" style="height: 1000px;">
+			</div>
+		</div>
+
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4>Dokumen Persyaratan Pendaftaran Nikah</h4>
+			</div>
+			<div class="panel-body" style="padding-left: 40px;">
+				<p><b>Dasar : PMA Nomor 20 Tahun 2019</b></p>
+				<p><b>Siapkan : </b></p>
+				<ul>
+					<li>NIK Calon Suami</li>
+					<li>NIK Calon Istri</li>
+					<li>NIK Orang Tua/Wali</li>
+				</ul>
+				<br>
+				<p><b>Berikut dokumen-dokumen yang dibutuhkan untuk daftar nikah : </b></p>
+				<p>N1 - Surat Pengantar Nikah (Didapat dari Kelurahan/Desa)</p>                       
+				<p>N3 - Surat Persetujuan Mempelai</p>
+				<p>N5 - Surat Izin Orang Tua (Jika calon pengantin umurnya dibawah 21 tahun)</p>                        
+				<p>Surat Akta Cerai (Jika calon pengantin sudah cerai)</p>
+				<p>Surat Izin Komandan (Jika calon pengantin TNI atau POLRI)</p>
+				<p>Surat Akta Kematian (Jika calon pengantin duda/janda ditinggal mati)</p>
+				<p>Izin/Dispensasi dari Pengadilan Agama Apabila : </p>
+				<ul>
+					<li>Calon Suami Kurang dari 19 Tahun</li>
+					<li>Calon Istri Kurang dari 19 Tahun</li>
+					<li>Izin Poligami</li>
+				</ul> 
+				<p>Izin dari Kedutaan Besar untuk WNA</p>
+				<p>Fotocopy Identitas Diri (KTP)</p>
+				<p>Fotocopy Kartu Keluarga</p>
+				<p>Fotocopy Akta Lahir</p>
+				<p>Surat Rekomendasi Nikah dari KUA Kecamatan (Jika nikah dilangsungkan di luar wilayah tempat tinggal catin)</p>
+				<p>Pasphoto ukuran 2 x 3 sebanyak 5 lembar</p>
+				<p>Pasphoto ukuran 4 x 6 sebanyak 2 lembar</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php      
+require('layout/footer.php');
+?>
---
+++ user/daftar.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Pengajuan Permintaan Duplikat Buku Nikah</title>
+    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="">
+    <meta name="author" content="Phoenixcoded" />
+    <!-- Favicon icon -->
+    <!-- <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon"> -->
+
+    <!-- vendor css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="main-body">
+            <div class="page-wrapper pt-5">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h2 class="text-center">Form Pengajuan Permintaan Duplikat Buku
+                                    Nikah</h2>
+                            </div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label class="floating-label">Periksa NIK anda</label>
+                                                <input type="number" class="form-control"
+                                                    placeholder="Masukkan NIK anda">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label class="floating-label"></label>
+                                                <div class="mt-2">
+                                                    <button class="btn btn-primary">Cek NIK</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<!-- Required Js -->
+<script src="assets/js/vendor-all.min.js"></script>
+<script src="assets/js/plugins/bootstrap.min.js"></script>
+
+<script src="assets/js/pcoded.min.js"></script>
+
+</body>
+
+</html>
